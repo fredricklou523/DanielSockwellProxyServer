fb3292c941e298a7bc383dc06936c95ea00af21e
Daniel Sockwell <dsockwell@gmail.com>
2018-11-07 14:18:38 -0500

    Add proxy server

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
+app.use(express.static(`${__dirname}./../client/dist`));
+
+app.get('/product/:productId', (req, res) => {
+  if (req.params.productId === 'random') {
+    res.redirect(`/product/${Math.floor(Math.random() * 100) + 1}`);
+  } else {
+    const options = { headers: { 'Content-Type': 'text/html' } };
+    const file = path.join(`${__dirname}./../index.html`);
+    res.sendFile(file, options);
+  }
+});
+
+module.exports = { app };
